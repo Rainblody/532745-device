8ce0e2fd5c4b4faff10aa89729c3aa1e493d05e2
Timanovski <Rainblody@gmail.com>
2018-06-29 14:15:30 +0400

    Первый вариант главной страницы

---
+++ js/script.js
@@ -0,0 +1,38 @@
+﻿var formLink = document.querySelector(".write-us-button");
+
+var formPopup = document.querySelector(".modal-1");
+var formClose = document.querySelector(".modal-letter-close");
+var formLogin = formPopup.querySelector("[name=name]");
+
+var mapLink = document.querySelector(".contacts-map");
+
+var mapPopup = document.querySelector(".modal-2");
+var mapClose = document.querySelector(".modal-map-close");
+  
+formLink.addEventListener("click", function (evt) {
+  "use strict";
+  evt.preventDefault();
+  formPopup.classList.add("modal-letter");
+  formLogin.focus();
+});
+
+formClose.addEventListener("click", function (evt) {
+  "use strict";
+  evt.preventDefault();
+  formPopup.classList.remove("modal-letter");
+});
+
+mapLink.addEventListener("click", function (evt) {
+  "use strict";
+  evt.preventDefault();
+  mapPopup.classList.add("modal-map");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  "use strict";
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-map");
+});
+
+
+
