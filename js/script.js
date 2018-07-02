f778161382ed9b23d89eeb831691454d98d69ba1
Timanovski <Rainblody@gmail.com>
2018-07-02 18:01:36 +0400

    Исправления

--- js/script.js
+++ js/script.js
@@ -1,35 +1,43 @@
 ﻿var formLink = document.querySelector(".write-us-button");
 
 var formPopup = document.querySelector(".modal-1");
-var formClose = document.querySelector(".modal-letter-close");
-var formLogin = formPopup.querySelector("[name=name]");
+var formClose = formPopup.querySelector(".modal-close");
+var formName = formPopup.querySelector("[name=name]");
+var email = formPopup.querySelector("[name=email]");
+var form = formPopup.querySelector("[name=letterForm]");
+var letter = formPopup.querySelector("[name=letter]");
 
 var mapLink = document.querySelector(".contacts-map");
 
 var mapPopup = document.querySelector(".modal-2");
-var mapClose = document.querySelector(".modal-map-close");
+var mapClose = mapPopup.querySelector(".modal-close");
   
 formLink.addEventListener("click", function (evt) {
-  "use strict";
+  
   evt.preventDefault();
   formPopup.classList.add("modal-letter");
-  formLogin.focus();
+  formName.focus();
 });
 
 formClose.addEventListener("click", function (evt) {
-  "use strict";
   evt.preventDefault();
   formPopup.classList.remove("modal-letter");
+  formPopup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!formName.value || !email.value || !letter.value) {
+    evt.preventDefault();
+    formPopup.classList.add("modal-error");
+  }
 });
 
 mapLink.addEventListener("click", function (evt) {
-  "use strict";
   evt.preventDefault();
   mapPopup.classList.add("modal-map");
 });
 
 mapClose.addEventListener("click", function (evt) {
-  "use strict";
   evt.preventDefault();
   mapPopup.classList.remove("modal-map");
 });
